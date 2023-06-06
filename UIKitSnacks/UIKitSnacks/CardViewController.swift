0933c1ba9e86ff694233a4003614a43fe5676f00
Sergey Zakharov <sergeyzakharov.dev@gmail.com>
2023-06-06 14:16:29 +0300

    [===] keyboard dismiss & input border

--- UIKitSnacks/UIKitSnacks/CardViewController.swift
+++ UIKitSnacks/UIKitSnacks/CardViewController.swift
@@ -7,25 +7,24 @@
 
 import UIKit
 
-class CardViewController: UIViewController {
+class CardViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet var items: UITextField!
     @IBOutlet var price: UILabel!
+    @IBOutlet weak var countBar: UIStackView!
     
-    @IBAction func editingChanged(_ sender: UITextField) {
-        if let text = self.items.text, let value = Double(text) {
-            
-        if value < 0 { return }
+    // MARK: - edit items count
+    @IBAction func changeCount(_ sender: UITextField) {
+        
+        if let text = self.items.text {
+            let value = text.isEmpty ? 0 : Double(text)!;
+                
+            if value < 0 { return }
             self.price.text = String(format: "$%.2f", 1.99 * value);
         }
     }
     
-    @IBAction func editingEnd(_ sender: UITextField) {
-        if let text = self.items.text, let value = Double(text) {
-            print("end: \(value)")
-        }
-    }
-    
+    // MARK: - add item to cart
     @IBAction func addItem(_ sender: UIButton) {
         if let text = self.items.text, let value = Int(text) {
 
@@ -36,7 +35,8 @@ class CardViewController: UIViewController {
         }
     }
     
-    @IBAction func removeItemBtn(_ sender: UIButton) {
+    // MARK: - remove item from cart
+    @IBAction func removeItem(_ sender: UIButton) {
         if let text = self.items.text, let value = Int(text) {
             if value == 0 { return }
             
@@ -49,8 +49,25 @@ class CardViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // adding border to count bar
+        countBar.layer.borderWidth = 2
+        countBar.layer.borderColor = CGColor(red: 25 / 255, green: 25 / 255, blue: 25 / 255, alpha: 0.1)
+        countBar.layer.cornerRadius = 6
+        
+        
+        // adding tap gesture responder to parent view
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(self.dismissKeyboard (_:)))
+        self.view.addGestureRecognizer(tapGesture)
     }
-
+    
+    // MARK: - dismiss keyboard on tap outside
+    @objc func dismissKeyboard(_ sender: UITapGestureRecognizer) {
+        items.resignFirstResponder()
+        
+        if let text = items.text {
+            if(text.isEmpty) { items.text = "0" }
+        }
+    }
 }
 
